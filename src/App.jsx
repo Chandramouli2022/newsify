6d3b39e18e8af1e1faf2041db34d00941c7062de
Mouli <chandramouli.panyam@gmail.com>
2025-03-23 19:38:20 +0530

    view toggle interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,212 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import im from "./assets/image.jpg";
+import close from "./assets/close.png";
+import user from "./assets/user.png";
+import group from "./assets/group.svg";
+import list from "./assets/list.svg";
+import timeImg from "./assets/time.png";
+import data from "./data.js"
 
-function App() {
-  const [count, setCount] = useState(0)
+function CardGroup({ id, title, description, time, image, onClose }) {
+  const maxLength = 50;
+  const maxLengthDescription = 60;
+  const trimmedTitle =
+    title.length > maxLength ? title.substring(0, maxLength) + "..." : title;
+  const trimmedDescription =
+    description.length > maxLengthDescription
+      ? description.substring(0, maxLengthDescription) + "..."
+      : description;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='card'>
+      <div className='card-close' onClick={() => onClose(id)}>
+        <img src={close} alt='close' className='close-image' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h2 className='card-title'>{trimmedTitle}</h2>
+      <p className='card-description'>{trimmedDescription}</p>
+      <p className='card-time'>
+        <img
+          src={timeImg}
+          alt='cal'
+          width={"8px"}
+          style={{ paddingRight: "4px" }}
+        />
+        {new Date(time).toLocaleString()}
       </p>
-    </>
-  )
+      {image && <img src={image} alt='news' className='card-image' />}
+    </div>
+  );
 }
 
-export default App
+function CardList({ id, title, description, time, image, onClose }) {
+  const maxLength = 70;
+  const maxLengthDescription = 80;
+  const trimmedTitle =
+    title.length > maxLength ? title.substring(0, maxLength) + "..." : title;
+  const trimmedDescription =
+    description.length > maxLengthDescription
+      ? description.substring(0, maxLengthDescription) + "..."
+      : description;
+
+  return (
+    <div className='card2'>
+      {image && <img src={image} alt='news' className='card-image' />}
+      <div className='card-details'>
+        <h2 className='card-title'>{trimmedTitle}</h2>
+        <p className='card-description'>{trimmedDescription}</p>
+        <p className='card-time'>{new Date(time).toLocaleString()}</p>
+      </div>
+      <div className='card-close' onClick={() => onClose(id)}>
+        <img src={close} alt='close' className='close-image' />
+      </div>
+    </div>
+  );
+}
+
+function PageButton({ n, isActive, onClick }) {
+  return (
+    <button
+      className={`page-number ${isActive ? "active" : ""}`}
+      onClick={() => onClick(n)}
+    >
+      {n}
+    </button>
+  );
+}
+
+function Cards({ currentArticles, view, onClose }) {
+  return view === "group" ? (
+    <div className='cards-group'>
+      {currentArticles.map((article) => (
+        <CardGroup key={article.id} {...article} onClose={onClose} />
+      ))}
+    </div>
+  ) : (
+    <div className='cards-list'>
+      {currentArticles.slice(0, 5).map((article) => (
+        <CardList key={article.id} {...article} onClose={onClose} />
+      ))}
+    </div>
+  );
+}
+var newsData = data;
+function App() {
+  const [articles, setArticles] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [view, setView] = useState("group");
+  const articlesPerPage = 6;
+
+  const API_URL =
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25be7d8287a84972a775f5252e6eabc1";
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        // const response = await axios.get(API_URL);
+        // newsData = response.data.articles.map((article) => ({
+        //   id: uuidv4(),
+        //   title: article.title || "No Title",
+        //   description: article.description || "No Description",
+        //   time: article.publishedAt || new Date().toISOString(),
+        //   image: article.urlToImage || im,
+        //   readMore: article.url || "https://biztoc.com/x/11e125609293b7f3",
+        // }));
+        //console.log(newsData);
+        setArticles(newsData.slice(0, 24)); // Store only 24 articles
+      } catch (error) {
+        console.error("Error fetching news articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = articles.slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  );
+  const totalPages = Math.ceil(articles.length / articlesPerPage);
+
+  const handleClose = (id) => {
+    setArticles((prevArticles) => {
+      const filteredArticles = prevArticles.filter(
+        (article) => article.id !== id
+      );
+      if (filteredArticles.length > 5) return [...filteredArticles];
+      else {
+        setArticles(newsData.slice(0, 24));
+        return articles
+      }
+    });
+  };
+
+  return (
+    <div className='main-page'>
+      <aside className='sidebar'>
+        <div className='user'>
+          <img src={user} alt='user' className='user-image' />
+          <span>
+            <h2 className='user-hi'>Hi Reader,</h2>
+            <p className='user-guide'>Here's your News!</p>
+          </span>
+        </div>
+        <div className='view-toggle'>
+          <h2 className='toggle-title'>View Toggle</h2>
+          <div className='toggle-container'>
+            <button
+              className={
+                view === "group" ? "toggle-group active" : "toggle-group"
+              }
+              onClick={() => setView("group")}
+            >
+              <img src={group} alt='group' className='toggle-image1' />
+            </button>
+            <button
+              className={view === "list" ? "toggle-list active" : "toggle-list"}
+              onClick={() => setView("list")}
+            >
+              <img src={list} alt='list' className='toggle-image2' />
+            </button>
+          </div>
+        </div>
+        <div className='feedback'>
+          <h2 className='feedback-title'>Have a Feedback?</h2>
+          <button className='feedback-button'>We’re Listening!</button>
+        </div>
+      </aside>
+      <main className='content'>
+        <Cards
+          currentArticles={currentArticles}
+          view={view}
+          onClose={handleClose}
+        />
+        <nav className='page-nums'>
+          {Array.from({ length: Math.min(3, totalPages) }, (_, i) => (
+            <PageButton
+              key={i + 1}
+              n={i + 1}
+              isActive={i + 1 === currentPage}
+              onClick={setCurrentPage}
+            />
+          ))}
+          {totalPages > 3 && (
+            <button
+              className='page-number'
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              {" "}
+              &gt;&gt;{" "}
+            </button>
+          )}
+        </nav>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,222 @@
+const data = [
+  {
+    id: "afb35e42-8d11-4e95-96e8-2c2404eb1364",
+    title:
+      "Tesla owners are trading in cars at record rates amid Musk backlash - The Washington Post",
+    description:
+      "The data comes as Tesla’s stock price has fallen sharply and vehicles and dealerships have faced vandalism over Musk’s role in the Trump administration.",
+    time: "2025-03-22T00:36:08Z",
+    image:
+      "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IZYQDOMEA2SXZBCR6DJE7GDWVY.JPG&w=1440",
+    readMore:
+      "https://www.washingtonpost.com/business/2025/03/21/tesla-trade-ins-elon-musk/",
+  },
+  {
+    id: "a9c904c2-df3b-4cd2-8cc6-9433b91d5239",
+    title:
+      "How Alnylam Is Taking On Pfizer In A Massive, Growing Market - Investor's Business Daily",
+    description:
+      "Alnylam added a gene silencer to the cardiomyopathy space this week, and will now rival Pfizer and BridgeBio.",
+    time: "2025-03-21T21:13:00Z",
+    image:
+      "https://www.investors.com/wp-content/uploads/2019/01/stock-pharmaceutical-07-adobe.jpg",
+    readMore:
+      "https://www.investors.com/news/technology/alnylam-stock-amvuttra-approval-cardiomyopathy-pfizer-bridgebio/",
+  },
+  {
+    id: "21edb07f-2115-499e-8ea0-eaebf768dc16",
+    title:
+      "Legal experts say Trump official broke law by saying 'Buy Tesla' stock but don't expect a crackdown - The Associated Press",
+    description:
+      "A Trump Cabinet secretary violated the law when he told TV viewers this week to buy Tesla stock, but it's not clear that anything will be done about it. That's a worry for key ethics law experts. The comment from U.S. Commerce Secretary Howard Lutnick came a …",
+    time: "2025-03-21T21:12:00Z",
+    image:
+      "https://dims.apnews.com/dims4/default/e116ae8/2147483647/strip/true/crop/6000x3375+0+312/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F01%2Fe4%2Fe0bb46b89a58a08851259082ce7f%2F82eeb0048f184c3aa79aec30d77781d4",
+    readMore:
+      "https://apnews.com/article/musk-trump-tesla-stock-lutnick-commerce-secretary-ethics-5a89c2f4a68a9470692630b5c56cffd6",
+  },
+  {
+    id: "fb683df3-712f-41d6-961f-e28785f3bee4",
+    title:
+      "London's Heathrow Airport to restart flights after fire causes closure - ABC News",
+    description: "No Description",
+    time: "2025-03-21T21:11:15Z",
+    image: "/src/assets/image.jpg",
+    readMore:
+      "https://abcnews.go.com/International/fire-electrical-substation-closes-heathrow-airport/story?id\\\\u003d120009870",
+  },
+  {
+    id: "d4a6ee33-188d-4f29-88a4-c14578c348ff",
+    title:
+      "Stock Market Today: Dow Jones, S&P 500 And Nasdaq All Close Higher After Final Burst (Live Coverage) - Investor's Business Daily",
+    description: "No Description",
+    time: "2025-03-21T21:10:00Z",
+    image: "/src/assets/image.jpg",
+    readMore:
+      "https://www.investors.com/market-trend/stock-market-today/nasdaq-sp500-dow-jones-berkshire-hathaway-nike-earnings-axsome-nvidia-palantir-axsome/",
+  },
+  {
+    id: "5b3720be-35c2-4f52-9cb8-97fb6e5bfd26",
+    title:
+      "JPMorgan quietly renames DEI program as White House wages war on woke corporate policies - New York Post ",
+    description:
+      "The memo was sent to staff by Chief Operating Officer Jenn Piepszak in which she said the bank sought to comply with “current laws and regulations.”",
+    time: "2025-03-21T21:10:00Z",
+    image:
+      "https://nypost.com/wp-content/uploads/sites/2/2025/03/100859514.jpg?quality=75&strip=all&w=1024",
+    readMore:
+      "https://nypost.com/2025/03/21/business/jpmorgan-renames-dei-scheme-amid-white-house-war-on-woke/",
+  },
+  {
+    id: "8edeb06a-2865-4ec5-92c5-c5001c8216ac",
+    title:
+      "JPMorgan CEO Jamie Dimon says he will remain CEO a few more years—and promises ‘no swearing this time’ at latest town hall - Fortune",
+    description: "JPMorgan Chase makes changes to DEI program",
+    time: "2025-03-21T20:45:00Z",
+    image:
+      "https://fortune.com/img-assets/wp-content/uploads/2025/03/dimon032125.jpg?resize=1200,600",
+    readMore:
+      "https://fortune.com/2025/03/21/jpmorgan-jamie-dimon-inflation-economy-return-to-work-wall-street-veterans-dei/",
+  },
+  {
+    id: "effc154a-3b8f-4bc1-8c64-114856509ff2",
+    title:
+      "Wall Street Investors With Diversified Bets Are Winning at Last - Bloomberg",
+    description:
+      "Never-ending market disruption is upending investment blueprints everywhere in 2025, while hitting sentiment across US stocks.",
+    time: "2025-03-21T20:42:11Z",
+    image:
+      "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i6K9Qol8Gazo/v0/1200x793.jpg",
+    readMore:
+      "https://www.bloomberg.com/news/articles/2025-03-21/wall-street-s-bear-market-in-diversification-is-finally-easing",
+  },
+  {
+    id: "099598a9-ed9f-43c0-a4c0-5a0a0abcded0",
+    title:
+      "StubHub files for IPO as companies start lining up to go public - CNBC",
+    description:
+      "StubHub has become the latest tech-related company to file its IPO prospectus with the U.S. Securities and Exchange Commission.",
+    time: "2025-03-21T20:28:46Z",
+    image:
+      "https://image.cnbcfm.com/api/v1/image/108119872-1742585340148-gettyimages-2149117394-_s1_3621_oenivfpt.jpeg?v=1742589693&w=1920&h=1080",
+    readMore: "https://www.cnbc.com/2025/03/21/stubhub-files-for-ipo.html",
+  },
+  {
+    id: "cd3a755b-3186-418a-a237-24179ac03474",
+    title:
+      "These Stocks Moved the Most Today: FedEx, Boeing, Nike, Micron, Super Micro, NIO, D-Wave Quantum, and More - Barron's",
+    description: "No Description",
+    time: "2025-03-21T20:28:00Z",
+    image: "/src/assets/image.jpg",
+    readMore: "https://www.barrons.com/articles/stock-market-movers-83e1eff2",
+  },
+  {
+    id: "6b6be798-a0d8-4313-b49b-3a9c12c3478c",
+    title:
+      "Nike shares plunge after sneaker giant warns sales could fall by double digits - New York Post ",
+    description:
+      "The sneaker giant has lost 5% of its value so far this year — following a 30% plunge in 2024.",
+    time: "2025-03-21T19:29:00Z",
+    image:
+      "https://nypost.com/wp-content/uploads/sites/2/2025/03/100848836-1.jpg?quality=75&strip=all&w=1024",
+    readMore:
+      "https://nypost.com/2025/03/21/business/nike-shares-plunge-after-sneaker-giant-warns-sales-could-fall-by-double-digits/",
+  },
+  {
+    id: "d2a25d9b-47af-48fb-b01d-b945e3a69de1",
+    title:
+      "Trump escalates threats against those who destroy Tesla vehicles - The Washington Post",
+    description:
+      "Trump wrote on social media that people who vandalize vehicles made by Tesla — the company owned by Trump ally Elon Musk — could get lengthy jail sentences.",
+    time: "2025-03-21T18:34:07Z",
+    image:
+      "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/RIUV7RVZE4Q22EN2DTI3MHPOSY_size-normalized.jpg&w=1440",
+    readMore:
+      "https://www.washingtonpost.com/national-security/2025/03/21/trump-tesla-vehicles-elon-musk-bondi-vandalism/",
+  },
+  {
+    id: "9710372f-917f-4367-b2b6-1657e033812e",
+    title:
+      "Chomps Issues Recall Over Metal Contamination in Popular Protein Sticks - Health.com",
+    description:
+      "The popular protein snack brand Chomps announced a recall of its Original Beef sticks and Original Turkey sticks after receiving customers complaints about metal fragments in the products.",
+    time: "2025-03-21T18:24:45Z",
+    image:
+      "https://www.health.com/thmb/btnyV7sXJ7yXBi555iYO8oS6efE=/4000x0/filters:no_upscale():max_bytes(150000):strip_icc()/Copyofrecall-template-purple7-39a3a6aa7bc942cfba59bf36d6365d3c.png",
+    readMore:
+      "https://www.health.com/chomps-recall-metal-contamination-11701274",
+  },
+  {
+    id: "d3e683f8-9781-4b7a-bcb4-c2fc9828433b",
+    title:
+      "'The Big Short' investor Danny Moses: Markets have 'underestimated' negative impact of DOGE cuts - Fortune",
+    description:
+      "The cost-cutting spree isn’t limited to government—billions in private-business contracts are affected, Danny Moses told Fortune.",
+    time: "2025-03-21T18:17:00Z",
+    image:
+      "https://fortune.com/img-assets/wp-content/uploads/2025/03/GettyImages-2201505740_377d94-e1742580297785.jpg?resize=1200,600",
+    readMore:
+      "https://fortune.com/2025/03/21/the-big-short-danny-moses-doge-stock-market-impact/",
+  },
+  {
+    id: "a12f68fc-7e87-43e6-8a07-5e028b4c6b57",
+    title:
+      "Here are the best deals you can already grab from Amazon’s spring sales event - The Verge",
+    description:
+      "Amazon’s latest sale isn’t a Prime Day event, but it’s still a great time to save. Here, we rounded up the best deals you can already get on earbuds, tablets, and more.",
+    time: "2025-03-21T18:00:50Z",
+    image:
+      "https://platform.theverge.com/wp-content/uploads/sites/2/2025/03/257633_amazon_big_spring_sale_CVirginia.jpg?quality=90&strip=all&crop=0%2C10.732984293194%2C100%2C78.534031413613&w=1200",
+    readMore:
+      "https://www.theverge.com/tech/632597/amazon-big-spring-sale-2025-prime-day-deals",
+  },
+  {
+    id: "212b334c-9f42-43af-98eb-b01f27496a65",
+    title:
+      "The salary you need to be considered middle class in every U.S. state - AOL",
+    description:
+      "A six-figure household income doesn’t necessarily make you rich — in many cases, it just means you’re middle class. The upper bound of what’s considered midd...",
+    time: "2025-03-21T17:24:35Z",
+    image:
+      "https://s.yimg.com/ny/api/res/1.2/qJzWITCDq5EHhHZlg4Iyog--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD03Nzg-/https://media.zenfs.com/en/aol_nbc_universal_184/5f0f803fa0d4acdee8293222e5a3ec28",
+    readMore:
+      "https://www.nbcnews.com/business/personal-finance/salary-need-considered-middle-class-every-us-state-rcna197508",
+  },
+  {
+    id: "7d66d363-dfb9-49a0-98ee-5a1bba9fdbdd",
+    title:
+      "Elon Musk tells Tesla employees 'hang onto your stock,' urges vandals to 'stop being psycho' - CNBC",
+    description:
+      "In an all-hands  livestreamed meeting on Thursday night, Tesla CEO Elon Musk commented on some of protests and instances of vandalism the company has faced.",
+    time: "2025-03-21T17:00:32Z",
+    image:
+      "https://image.cnbcfm.com/api/v1/image/108114305-1742584083907-108114305-17417212502025-03-11t192114z_254284279_rc26bdah68f8_rtrmadp_0_usa-trump.jpeg?v=1742584102&w=1920&h=1080",
+    readMore:
+      "https://www.cnbc.com/2025/03/21/elon-musk-tells-tesla-employees-hang-onto-your-stock-in-all-hands.html",
+  },
+  {
+    id: "6588ffd8-2ff8-4894-bf58-d3bc39eeaffc",
+    title:
+      "Micron Stock Falls but Analysts Are Bullish Despite Gross Margin 'Fly in the Ointment' - Investopedia",
+    description:
+      "Shares of Micron Technology turned lower Friday after the company’s fiscal second-quarter results a day earlier, but analysts have remained bullish on the memory chip company.",
+    time: "2025-03-21T16:32:10Z",
+    image:
+      "https://www.investopedia.com/thmb/4qah7MMw7e3AmIx5oIReXMzfDP4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-21479041531-de0db4ad72954ef8bf329e3f15580a75.jpg",
+    readMore:
+      "https://www.investopedia.com/micron-stock-falls-but-analysts-are-bullish-despite-gross-margin-fly-in-the-ointment-11701189",
+  },
+  {
+    id: "613ce92a-6dcf-42b0-b1eb-b1f6bc38fafc",
+    title:
+      "Nvidia's CEO sought to help the quantum companies and ended up causing another sell-off in the stocks - CNBC",
+    description: "Several quantum stocks ended Thursday in the red.",
+    time: "2025-03-21T16:18:34Z",
+    image:
+      "https://image.cnbcfm.com/api/v1/image/108108037-17406068842025-01-07t041321z_2046107004_rc2s4caqeorx_rtrmadp_0_tech-ces-nvidia.jpeg?v=1741627841&w=1920&h=1080",
+    readMore:
+      "https://www.cnbc.com/2025/03/21/nvidias-ceo-tried-to-help-the-quantum-companies-but-a-sell-off-in-the-stocks-ensued.html",
+  },
+];
+
+export default data;
